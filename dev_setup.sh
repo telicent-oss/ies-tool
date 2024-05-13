962b6c6d9b9f6f41916f26661e8729ed060f0693
Daniel Davies <110399083+danieldaviestelicent@users.noreply.github.com>
2024-05-13 13:22:20 +0100

    build: updated pyshacl

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,3 +1,5 @@
 python3 -m pip install --upgrade pip
 python3 -m pip install pip-tools
 python3 -m piptools compile --extra dev -o requirements.txt pyproject.toml
+pip3 install -r requirements.txt
+pre-commit install
