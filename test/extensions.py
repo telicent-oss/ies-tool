fb2b5404f0738487c241902c5b9af6bd5f58c252
Andreas Cola <andreas.cola@telicent.io>
2025-05-16 10:37:30 +0100

    chore: add code to test ExchangePayload bug

--- test/extensions.py
+++ test/extensions.py
@@ -1,4 +1,4 @@
-from ies_tool.ies_tool import IES_TOOL, Device, RdfsResource
+from ies_tool.ies_tool import IES_TOOL, IESTool
 
 ADDITONAL_CLASSES = {
     "http://ies.data.gov.uk/ontology/ies4#Widget": ['http://ies.data.gov.uk/ontology/ies4#Device'],
@@ -7,3 +7,6 @@ ADDITONAL_CLASSES = {
 
 
 IES_TOOL.save_rdf("extension.ttl",format="ttl")
+
+tool = IESTool(mode="rdflib")
+ep = tool.instantiate(classes=["http://ies.data.gov.uk/ontology/ies4#ExchangePayload"])
