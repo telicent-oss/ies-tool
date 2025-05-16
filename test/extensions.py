37e3d8337fe70378cc9eb6c3f9065b8217e2621d
MonkeyChap <956361+MonkeyChap@users.noreply.github.com>
2025-05-16 15:10:30 +0100

    Tests

--- test/extensions.py
+++ test/extensions.py
@@ -1,12 +1,11 @@
-from ies_tool.ies_tool import IES_TOOL, IESTool
+from ies_tool.ies_tool import IES_TOOL, IESTool, Device
 
 ADDITONAL_CLASSES = {
     "http://ies.data.gov.uk/ontology/ies4#Widget": ['http://ies.data.gov.uk/ontology/ies4#Device'],
     "http://ies.data.gov.uk/ontology/ies4#Gizmo": ['http://ies.data.gov.uk/ontology/ies4#Device']
 }
 
+tool = IESTool()
 
-IES_TOOL.save_rdf("extension.ttl",format="ttl")
-
-tool = IESTool(mode="rdflib")
 ep = tool.instantiate(classes=["http://ies.data.gov.uk/ontology/ies4#ExchangePayload"])
+tool.save_rdf("extension.ttl",rdf_format="ttl")
