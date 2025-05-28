64bf32ecfee46b1b43847a90b5a33dcfd88e161c
MonkeyChap <956361+MonkeyChap@users.noreply.github.com>
2025-05-28 15:21:10 +0100

    testing precision

--- test/extensions.py
+++ test/extensions.py
@@ -1,5 +1,5 @@
-from ies_tool.ies_tool import Device, IESTool
-
+from ies_tool.ies_tool import Device, GeoPoint, IESTool
+from geohash_tools import encode
 ADDITONAL_CLASSES = {
     "http://ies.data.gov.uk/ontology/ies4#Widget": ['http://ies.data.gov.uk/ontology/ies4#Device'],
     "http://ies.data.gov.uk/ontology/ies4#Gizmo": ['http://ies.data.gov.uk/ontology/ies4#Device']
@@ -13,4 +13,13 @@ print(type(ep))
 dev1 = tool.instantiate(classes=["http://ies.data.gov.uk/ontology/ies4#Widget"])
 print(type(dev1))
 dev2 = Device(tool=tool)
+lat = 23.4567891234
+lon = 12.3456891234
+GeoPoint(
+    tool=tool,
+    lat=lat,
+    lon=lon
+)
+base_uri = "http://geohash.org/" + str(encode(float(lat), float(lon), precision=6))
+print(base_uri)
 tool.save_rdf("extension.ttl",rdf_format="ttl")
